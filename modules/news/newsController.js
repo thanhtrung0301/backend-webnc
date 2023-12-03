569538c67d159ce697b068699527bf959b39d877
nttrung0301 <trung-nguyenthanh@outlook.com>
2023-12-03 17:52:20 +0700

    fix crawl

--- modules/news/newsController.js
+++ modules/news/newsController.js
@@ -1,43 +1,40 @@
 const newsController = {};
 const newsSchema = require("./newsSchema");
-const cheerio = require('cheerio');
-const request = require('request-promise');
-const moment = require('moment');
-
+const cheerio = require("cheerio");
+const request = require("request-promise");
+const moment = require("moment");
 
 newsController.getCrawlNews = async (req, res, next) => {
   try {
     const list = await newsController.getContent();
     if (list.length === 0) {
-      console.log('call api err');
+      console.log("call api err");
       return;
     }
     for (let i of list) {
       const one = new newsSchema({
-        categories: [
-          {
-            name: 'Xã hội',
-            url: 'xa-hoi',
-            slug: 'xa-hoi',
-          },
-        ],
-        img_hor: i.url_img ? i.url_img : '',
+        category_name: i.category,
+        img_hor: i.url_img ? i.url_img : "",
         description: i.description,
         title_google: i.title,
-        url: i.link.replace('https://vnexpress.net/', '').replace('.html', ''),
+        url: i.link.replace("https://vnexpress.net/", "").replace(".html", ""),
         content: i.content,
         title: i.title,
-        post_url: i.link.replace('https://vnexpress.net', '').replace('.html', ''),
-        title_search: i.link.replace('https://vnexpress.net/', '').replace('.html', ''),
+        post_url: i.link
+          .replace("https://vnexpress.net", "")
+          .replace(".html", ""),
+        title_search: i.link
+          .replace("https://vnexpress.net/", "")
+          .replace(".html", ""),
         news_id: i.news_id,
-        publishedAt: moment().format(),
-        tags: [{ label: 'tin mới', slug: 'tin-moi' }],
-        from_source: 'vnexpress.net',
+        publishedAt: moment(i.publishedAt, "DD/MM/YYYY, HH:mm"),
+        tags: [{ label: "tin mới", slug: "tin-moi" }],
+        from_source: "vnexpress.net",
       });
       await one.save();
     }
     return res.status(200).send({
-      message: "Successfully!"
+      message: "Successfully!",
     });
   } catch (error) {
     console.log(error);
@@ -46,20 +43,41 @@ newsController.getCrawlNews = async (req, res, next) => {
 
 newsController.getContent = async () => {
   try {
-    const list = await newsController.LishLink(`https://vnexpress.net/tin-tuc-24h`);
+    const list = await newsController.LishLink(
+      `https://vnexpress.net/tin-tuc-24h`
+    );
     let newLinks = [];
     for (let i of list) {
       if (i.link) {
         let html = await request(i.link);
         let $ = cheerio.load(html); // load HTML
-        let data = '';
-        $('section.section.page-detail.top-detail > div.container > div.sidebar-1 > article.fck_detail').each((index, testEl) => {
+        let data = "";
+        let publishedAt = "";
+        let category = "";
+        $(
+          "section.section.page-detail.top-detail > div.container > div.sidebar-1 > div.header-content.width_common > span.date"
+        ).each((index, testEl) => {
+          publishedAt = $(testEl).text();
+          const startPos = publishedAt.indexOf(", ");
+          const endPos = publishedAt.indexOf(" (");
+          publishedAt = publishedAt.slice(startPos + 2, endPos);
+        });
+        $(
+          "section.section.page-detail.top-detail > div.container > div.sidebar-1 > div.header-content.width_common > ul > li > a"
+        ).each((index, testEl) => {
+          category = $(testEl).text();
+        });
+        $(
+          "section.section.page-detail.top-detail > div.container > div.sidebar-1 > article.fck_detail"
+        ).each((index, testEl) => {
           data = $(testEl).html();
-          const cutPoint = data.indexOf(`<p class="Normal" style="text-align:right;">`);
-          data = data.slice(0, cutPoint) + '</article>';
+          const cutPoint = data.indexOf(
+            `<p class="Normal" style="text-align:right;">`
+          );
+          data = data.slice(0, cutPoint) + "</article>";
         });
         if (data) {
-          newLinks.push({ ...i, content: data, category: ['5f92ec4e2232780205439907'] });
+          newLinks.push({ ...i, content: data, category, publishedAt });
         }
         // await sleep(TIME_CRAWL_NEWS);
       }
@@ -72,24 +90,33 @@ newsController.getContent = async () => {
 
 newsController.LishLink = async (page) => {
   try {
-    const listAllNews = await newsSchema.find().select('news_id').sort({ publishedAt: -1 });
+    const listAllNews = await newsSchema
+      .find()
+      .select("news_id")
+      .sort({ publishedAt: -1 });
     let html = await request(page);
     let $ = cheerio.load(html); // load HTML
     let links = [];
-    $('div.width_common.list-news-subfolder > article.item-news.item-news-common').each((index, item) => {
+    $(
+      "div.width_common.list-news-subfolder > article.item-news.item-news-common"
+    ).each((index, item) => {
       let itemAdd = {};
       $(item)
-        .find('h3.title-news > a')
+        .find("h3.title-news > a")
         .each((index1, title) => {
           itemAdd = {
             ...itemAdd,
-            link: $(title).attr('href'),
+            link: $(title).attr("href"),
             title: $(title).text(),
-            news_id: $(title).attr('href').replace('https://', '').replace('.html', '').slice(-7),
+            news_id: $(title)
+              .attr("href")
+              .replace("https://", "")
+              .replace(".html", "")
+              .slice(-7),
           };
         });
       $(item)
-        .find('p.description > a')
+        .find("p.description > a")
         .each((index1, title) => {
           itemAdd = {
             ...itemAdd,
@@ -97,11 +124,11 @@ newsController.LishLink = async (page) => {
           };
         });
       $(item)
-        .find('div.thumb-art > a.thumb.thumb-5x3 > picture > img')
+        .find("div.thumb-art > a.thumb.thumb-5x3 > picture > img")
         .each((index1, title) => {
           itemAdd = {
             ...itemAdd,
-            url_img: $(title).attr('data-src') || $(title).attr('src'),
+            url_img: $(title).attr("data-src") || $(title).attr("src"),
           };
         });
       const check = listAllNews.filter((e) => e.news_id === itemAdd.news_id);
